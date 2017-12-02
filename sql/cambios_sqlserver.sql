2a21d8736c01a626868abbe89428564f4f838305
Sergio Arriagada <sergio.arriagada@info-sys.cl>
2017-12-02 14:15:39 -0300

    Creacion BASE CENTRO COSTO

--- sql/cambios_sqlserver.sql
+++ sql/cambios_sqlserver.sql
@@ -5,3 +5,13 @@ idempresa int
 
 insert into rem_usuario_empresa (idusuario,idempresa) values (6,17);
 update rem_app set nombre = 'Mantenci&oacute;n Personal' where id = 10;
+
+
+create table rem_centro_costo (
+id int identity,
+idempresa int,
+nombre varchar(100),
+codigo varchar(10),
+valido tinyint,
+fecha datetime default getdate()
+)	
