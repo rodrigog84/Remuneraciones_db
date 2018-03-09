03d6d45c790b17f038baafcc19bf5335b22e6262
Sergio Arriagada <sergio.arriagada@info-sys.cl>
2018-03-09 11:27:53 -0300

    Se incorporan Contratos

---
+++ application/views/rrhh/contrato_colaborador.php
@@ -0,0 +1,7 @@
+<?php
+echo 'hola';
+echo $rut;
+
+?>
+
+<a href="" class="btn btn-info opciones" id="opciones" title="Contrato"><i class="fa fa-pencil-square-o" aria-hidden="true" role="button"></i></a>
