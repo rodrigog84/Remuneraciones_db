579c30fe151a60d8b4f307975e7c5264a1dbc845
Sergio Arriagada <sergio.arriagada@info-sys.cl>
2018-08-08 15:31:33 -0400

    Cambio Ip Base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -54,7 +54,8 @@ $db['default']['port'] = 1433;
 $db['default']['username'] = 'test'; 
 $db['default']['password'] = '1234';
 */
-$db['default']['hostname'] = '190.162.65.174';   // BD Remota
+//$db['default']['hostname'] = '190.162.65.174';   // BD Remota
+$db['default']['hostname'] = '201.239.216.55';   // BD Remota
 $db['default']['port'] = 1433; 
 $db['default']['username'] = 'usuarios_remuneraciones'; 
 $db['default']['password'] = '1QAZxsw2';
