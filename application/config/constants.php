125100921f714812aff85ca762e518c05da6d256
rodrigog84 <rodrigog.84@gmail.com>
2024-03-06 17:45:40 -0300

    cambio porcentajes fonasa

--- application/config/constants.php
+++ application/config/constants.php
@@ -47,8 +47,8 @@ define('ENVIO_MAIL',		TRUE);
 
 
 
-define('PORCT_FONASA',		0.039); //3.9% //1% //3.9%
-define('PORCT_INP',		0.031); //3.1%  //6% //3.1%
+define('PORCT_FONASA',		0.021); //3.9% //1% //3.9% // 2.1%
+define('PORCT_INP',		0.049); //3.1%  //6% //3.1% // 4.9 %
 
 define('PORCT_ISL',		0.93); //3.1%  //6%
 
